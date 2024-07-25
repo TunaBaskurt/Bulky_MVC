053d4aa308417b588f9381250ad37d078263c974
TunaBaskurt <tunayagmurtb@outlook.com>
2024-07-25 18:17:33 +0300

    Createing Data Base Table

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -4,8 +4,9 @@ namespace BulkyWeb.Models
 {
     public class Category
     {
-        [Key]
-        public int Id { get; set; }
+        
+        public int Category21Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
     }
