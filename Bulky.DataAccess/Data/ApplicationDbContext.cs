f2281902697f0be19618fe06d8cfca037d4ebff4
TunaBaskurt <tunayagmurtb@outlook.com>
2024-09-08 12:36:36 +0300

    Scaffold Identity has been added

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
 ﻿
 using Bulky.Models.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 //Veritabanı bağlamını temsil eder ve Entity Framework Core ile veritabanı işlemlerini yönetir.
 namespace Bulky.DataAccess.Data
 {
-    public class ApplicationDbContext :DbContext
+    public class ApplicationDbContext :IdentityDbContext
     {
        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options) 
         { 
@@ -16,6 +17,8 @@ namespace Bulky.DataAccess.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+
+            base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<Category>().HasData(
                 new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
                 new Category { Id = 2, Name = "Sci-Fi", DisplayOrder = 2 },
